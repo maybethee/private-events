d81539b39384f774e8cdea87ba7a95b7703d289d
maybethee <jozalamun@gmail.com>
2024-04-30 16:18:36 -0400

    Add links to user profile and return to index

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :attendances, foreign_key: 'attended_event_id'
   has_many :attendees, through: :attendances
+  scope :past, -> { where(date: (..(Time.now.midnight - 1.day))) }
+  scope :upcoming, -> { where(date: ((Time.now.midnight - 1.day)..)) }
 end
