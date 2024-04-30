122c774f4d52330eea9d5aeb7b3695e305239563
maybethee <jozalamun@gmail.com>
2024-04-30 12:18:53 -0400

    Build new event form associating User as creator

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,28 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date)
+  end
 end
+
